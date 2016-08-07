9d0dcd1964acdf20e380a7ef71313bb82d60ebd3
Tony Pelaez <tony@pelaez.me>
2016-08-07 09:26:21 -0700

    update style of recipe

--- src/Recipie/Fermentables/index.js
+++ src/Recipie/Fermentables/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import Qty from 'js-quantities';
+import { Button } from 'react-bootstrap';
 
 const amountToLb = (amount) => {
   let qty = new Qty(amount + 'kg');
@@ -7,20 +8,25 @@ const amountToLb = (amount) => {
 }
 
 export default ({fermentables}) => (
-  <table>
-    <thead>
-    <tr>
-      <th>Fermentable</th>
-      <th>Weight</th>
-    </tr>
-    </thead>
-    <tbody>
-    {fermentables.map((fermentable) =>
-       <tr key={fermentable.name}>
-         <td>{fermentable.name}</td>
-         <td>{amountToLb(fermentable.amount)}</td>
-       </tr>
-     )}
-  </tbody>
+  <div className="span6">
+    <h2>Fermentables</h2>
+    <table className="table table-hover table-bordered table-condensed">
+      <thead>
+        <tr>
+          <th>Fermentable</th>
+          <th>Weight</th>
+          <th></th>
+        </tr>
+      </thead>
+      <tbody>
+      {fermentables.map((fermentable) =>
+        <tr key={fermentable.name}>
+          <td>{fermentable.name}</td>
+          <td>{amountToLb(fermentable.amount)}</td>
+          <td><Button className="btn-mini"><i className="icon-trash" /></Button></td>
+        </tr>
+      )}
+    </tbody>
   </table>
+</div>
 );
--- src/Recipie/index.js
+++ src/Recipie/index.js
@@ -2,11 +2,11 @@ import React from 'react';
 import Fermentables from './Fermentables'
 
 export default ({recipie, ...props}) => (
-  <div>
+  <div className="row">
     <h1>
       {recipie.name}
     </h1>
-    <ul>
+    <ul className="unstyled">
       <li> Batch Size: {recipie.batch_size} </li>
       <li> Boil Size: {recipie.boil_size} </li>
     <Fermentables fermentables={recipie.fermentables} />
