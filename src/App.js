3113e589f1cec78795d7d67b31f25c34e722e2be
Tony Pelaez <tony@pelaez.me>
2016-08-07 10:35:12 -0700

    add button to toggle visibility of state for debugging

--- src/App.js
+++ src/App.js
@@ -1,4 +1,6 @@
 import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import classNames from 'classnames';
 import './App.css';
 import Recipie from './Recipie';
 
@@ -9,13 +11,26 @@ PaleAle = PaleAle.recipes[0];
 class App extends Component {
   constructor(props) {
     super(props);
+    PaleAle.dataIn = false;
     this.state = PaleAle;
+
+    this.toggleData = this.toggleData.bind(this);
+  }
+  toggleData() {
+    this.setState({dataIn: !this.state.dataIn});
   }
   render() {
+    const dataClass = classNames({
+      in: this.state.dataIn,
+      collapse: true,
+    });
     return (
       <div className="container App">
         <Recipie recipie={this.state} />
-        <div>
+        <div className="text-left" >
+          <Button onClick={ this.toggleData }>View State</Button>
+        </div>
+        <div className={dataClass}>
           <pre className="text-left">
             {JSON.stringify(this.state, null, 2)}
           </pre>
