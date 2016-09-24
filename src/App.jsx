6bfb766a81e6fa91eb6f386464b3766ffdf835ff
Tony Pelaez <tony@pelaez.me>
2016-09-23 22:23:09 -0700

    add app

---
+++ src/App.jsx
@@ -0,0 +1,43 @@
+import React, { Component } from 'react';
+import { Button } from 'react-bootstrap';
+import classNames from 'classnames';
+import './App.css';
+import Recipe from './Recipe';
+
+var PaleAle = require('json!../paleAle.json');
+
+PaleAle = PaleAle.recipes[0];
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    PaleAle.dataIn = false;
+    this.state = PaleAle;
+
+    this.toggleData = this.toggleData.bind(this);
+  }
+  toggleData() {
+    this.setState({dataIn: !this.state.dataIn});
+  }
+  render() {
+    const dataClass = classNames({
+      in: this.state.dataIn,
+      collapse: true,
+    });
+    return (
+      <div className="container App">
+        <Recipe recipe={this.state} />
+        <div className="text-left" >
+          <Button onClick={ this.toggleData }>View State</Button>
+        </div>
+        <div className={dataClass}>
+          <pre className="text-left">
+            {JSON.stringify(this.state, null, 2)}
+          </pre>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
