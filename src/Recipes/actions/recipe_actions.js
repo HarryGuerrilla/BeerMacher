1e55f453a4a850b898a498b4b8da7eb8b0c8793b
Tony Pelaez <tony@pelaez.me>
2017-03-20 22:04:02 -0700

    add firebase as datbase

--- src/Recipes/actions/recipe_actions.js
+++ src/Recipes/actions/recipe_actions.js
@@ -1,16 +1,22 @@
-import * as data from '../../assets/paleAle.json'
+import database from '../../Database'
 
 export const SELECT_RECIPE = 'SELECT_RECIPE'
 
 export function selectRecipe(recipe_id) {
-  let recipe = data.recipes.find((recipe) => {
-    return recipe.id === recipe_id
-  })
+  let recipe_url = `/users/recipes/${recipe_id - 1}`
 
-  console.log('fetching recipe...', recipe.name)
+  console.log('fetching recipe...')
 
-  return {
-    type: SELECT_RECIPE,
-    recipe
+  return dispatch => {
+    return database.ref(recipe_url).once('value', snap => {
+      let recipe = snap.val()
+      console.log(recipe.name)
+      dispatch(
+        {
+          type: SELECT_RECIPE,
+          recipe
+        }
+      )
+    })
   }
 }
--- src/index.js
+++ src/index.js
@@ -3,7 +3,8 @@ import ReactDOM from 'react-dom';
 import { Router, Route, IndexRoute, browserHistory } from 'react-router';
 import { syncHistoryWithStore } from 'react-router-redux';
 import { Provider } from 'react-redux';
-import { createStore } from 'redux';
+import { createStore, applyMiddleware } from 'redux';
+import thunk from 'redux-thunk';
 import App from './App';
 import Home from './Home';
 import Recipe from './Recipes';
@@ -11,7 +12,11 @@ import reducers from './reducers';
 import './index.css';
 import './bootstrap/css/bootstrap.min.css';
 
-const store = createStore(reducers);
+const store = createStore(
+  reducers,
+  {},
+  applyMiddleware(thunk)
+);
 const history = syncHistoryWithStore(browserHistory, store);
 
 ReactDOM.render(
