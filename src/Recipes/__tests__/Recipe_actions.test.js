829dac9beeb725e65c2151167b12dbcbdabfa847
Tony Pelaez <tony@pelaez.me>
2017-03-25 09:58:45 -0700

    add test for new action to get recipe names

--- src/Recipes/__tests__/Recipe_actions.test.js
+++ src/Recipes/__tests__/Recipe_actions.test.js
@@ -1,16 +1,47 @@
-import { SELECT_RECIPE } from '../actions/recipe_actions'
-import * as actions from '../actions/recipe_actions'
+import configureStore from 'redux-mock-store';
+
+
+import { SELECT_RECIPE, GET_ALL_RECIPE_NAMES } from '../actions/recipe_actions'
+//import * as actions from '../actions/recipe_actions'
 import * as data from '../../assets/paleAle.json'
+
+const middlewares = []
+const mockStore = configureStore(middlewares)
 
 describe('actions', () => {
   it('should select recipe', () => {
     const recipe = data.recipes[0]
+
+    const initialState = {}
+    const store = mockStore(initialState)
+
+    store.dispatch({ type: SELECT_RECIPE, recipe: recipe })
 
     const expectedAction = {
       type: SELECT_RECIPE,
       recipe
     }
 
-    expect(actions.selectRecipe(recipe.id)).toEqual(expectedAction)
+    const actions = store.getActions()
+
+    expect(actions).toEqual([expectedAction])
+  })
+
+  it('should select recipe names', () => {
+    const recipe_names = data.recipes.map(({name}, id) => ({name, id}))
+
+    const initialState = {}
+    const store = mockStore(initialState)
+
+    store.dispatch({ type: GET_ALL_RECIPE_NAMES, recipe_names: recipe_names })
+
+    const expectedAction = {
+      type: GET_ALL_RECIPE_NAMES,
+      recipe_names
+    }
+
+    const actions = store.getActions()
+
+    expect(actions).toEqual([expectedAction])
   })
 })
