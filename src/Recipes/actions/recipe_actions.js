57a1a7b2a74e6b97c5724c068610dc1ec5ff7c97
Tony Pelaez <tony@pelaez.me>
2017-03-25 10:09:05 -0700

    refactor

--- src/Recipes/actions/recipe_actions.js
+++ src/Recipes/actions/recipe_actions.js
@@ -4,13 +4,13 @@ export const SELECT_RECIPE = 'SELECT_RECIPE'
 export const GET_ALL_RECIPE_NAMES = 'GET_ALL_RECIPE_NAMES'
 
 export function selectRecipe(recipe_id) {
-  let recipe_url = `/users/recipes/${recipe_id}`
+  const recipe_url = `/users/recipes/${recipe_id}`
 
   console.log('fetching recipe...')
 
   return dispatch => {
     return database.ref(recipe_url).once('value', snap => {
-      let recipe = snap.val()
+      const recipe = snap.val()
       console.log(recipe.name)
       dispatch(
         {
@@ -23,13 +23,13 @@ export function selectRecipe(recipe_id) {
 }
 
 export function getAllRecipeNames() {
-  let recipes_url = '/users/recipes'
+  const recipes_url = '/users/recipes'
 
   console.log('fetching all recipe names')
 
   return dispatch => {
     return database.ref(recipes_url).once('value', snap => {
-      let recipe_names = snap.val().map(({name}, id) => ({name, id}))
+      const recipe_names = snap.val().map(({name}, id) => ({name, id}))
       dispatch({
         type: GET_ALL_RECIPE_NAMES,
         recipe_names
